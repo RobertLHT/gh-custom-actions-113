f0e9115e15872adf9dfc27c268b53fa5f1acf425
Haotian <Robertlht0828@gmail.com>
2024-03-30 15:57:31 -0400

    自动化上传界面

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -4,6 +4,15 @@ const github = require('@actions/github');
 
 
 function run() {
+    // 1. Get some inputs values
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', {  required: true });
+
+    // 2. upload files
+    const s3Url = `s3://${bucket}`
+    exec.exec('aws s3 sync ${distFolder} ${s3Url} --region ${bucketRegion}');
+
     core.notice("Hello from the deploy-s3-javascript action!");
 }
 
