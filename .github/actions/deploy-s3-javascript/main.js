c822a13d99cc533699d1cf8f7ec1f59c11f3ea42
Haotian <Robertlht0828@gmail.com>
2024-03-30 16:17:00 -0400

    add

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -10,10 +10,11 @@ function run() {
     const distFolder = core.getInput('dist-folder', {  required: true });
 
     // 2. upload files
-    const s3Url = `s3://${bucket}`
-    exec.exec(`aws s3 sync ${distFolder} ${s3Url} --region ${bucketRegion}`);
-
-    core.notice("Hello from the deploy-s3-javascript action!");
+    const s3Url = `s3://${bucket}` 
+    exec.exec(`aws s3 sync ${distFolder} ${s3Url} --region ${bucketRegion}`); //这里要用反斜杠
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl);
+    core.notice("Web url (from JS file): " + websiteUrl);
 }
 
 run();
